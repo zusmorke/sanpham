8ce997b418934629954644863435bb9f6b481c86
zusmorkeú <Dinhlove2k4@gmail.com>
2024-03-07 08:44:50 +0700

    haa

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/demo/{name}', function ($name) {
+    return 'Xin Chào' . " " . $name;
+})->whereAlpha('name');
